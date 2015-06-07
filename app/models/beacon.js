ac197c9f185736a7cdf0144a4d1075cfd2f622ea
Matthew Mazzuca <matthew.mazzuca@mail.utoronto.ca>
2015-06-07 17:30:13 -0400

    property edits and beacon adapter

--- app/models/beacon.js
+++ app/models/beacon.js
@@ -1,5 +1,22 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
-});
+	name: DS.attr('string'),
+	service_id: DS.attr('string')
+}).reopenClass({
+	FIXTURES: [
+	{
+		id: 1,
+		service_id: "#AK87IJU6",
+		name: "Living Room"
+	}, {
+		id: 2,
+		service_id: "#AK871JA9",
+		name: "Basement",
+	}, {
+		id: 3,
+		service_id: "#AK977B2P",
+		name: "Kitchen"
+	}
+	]
+})
---
+++ app/routes/beacons.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	model: function(params){
+		var propID = this.modelFor('property').get('id');
+
+		return this.store.all('beacon');
+	}
+});
